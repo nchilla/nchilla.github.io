9783f3c71027b0dfbc131f520404a3b2e4d219db
Nico <jnico.chilla@gmail.com>
2021-11-27 16:38:43 -0500

    basic work page setup

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,54 @@
+const yaml = require("js-yaml");
+const marked = require('marked');
+const fs = require('fs');
+let aspectratios;
+
+
+async function getVars(){
+  fs.readFile('input/_data/aspectratios.json',(err, data) => {
+    if (err) throw err;
+    aspectratios=JSON.parse(data);
+  })
+}
+
 module.exports = function(eleventyConfig) {
+
+  getVars();
   // Add a filter using the Config API
 
-  // eleventyConfig.addPassthroughCopy("templates/js");
+  eleventyConfig.addPassthroughCopy("input/js");
+  eleventyConfig.addPassthroughCopy("input/assets/video");
+  eleventyConfig.addPassthroughCopy("input/assets/fonts");
+  eleventyConfig.addPassthroughCopy("input/assets/images");
 
-  eleventyConfig.addFilter( "myFilter", function() {});
+  eleventyConfig.addNunjucksFilter( "md", function(value) {
+    var result;
+      try {
+        result = marked.parseInline(value);
+        return result;
+      } catch (e) {
+        return e;
+      }
+  });
+
+  eleventyConfig.addNunjucksFilter( "getRatio", function(value) {
+    return aspectratios.find(a=>a.name==value).ratio;
+  });
+
+
+
   eleventyConfig.setTemplateFormats("html,css,njk,ttf");
 
+  eleventyConfig.addDataExtension("yaml", contents => yaml.load(contents));
+  // eleventyConfig.addDataExtension("yaml", contents => console.log(yaml.load(contents)));
 
 
   // You can return your Config object (optional).
   return {
     dir: {
       input: "input",
-      output: "site"
+      output: "site",
+      data:'_data'
     }
   };
 };
---
+++ site/js/fast-track.js
@@ -0,0 +1,16 @@
+console.log('hello world');
+
+const observer = lozad(); // lazy loads elements with default selector as '.lozad'
+observer.observe();
+
+window.addEventListener('load',function(){
+  updateRefWidth();
+})
+window.addEventListener('resize',updateRefWidth);
+
+
+function updateRefWidth(){
+  const ref=document.querySelector('.reference');
+  const w=document.querySelector('.reference').offsetWidth;
+  document.documentElement.style.setProperty('--refwidth', w + 'px');
+}
